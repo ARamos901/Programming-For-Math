66e7f74a2e1b0e023970f9a4a9e7636923338182
ARamos901 <136763187+ARamos901@users.noreply.github.com>
2025-01-23 12:55:35 -0500

    Add files via upload

---
+++ 01.16.py
@@ -0,0 +1,21 @@
+#Aiden Ramos
+#Math 261
+#Class Code 01/16/2024
+
+
+
+#x=7
+#print(x)
+#x=1+x
+#print(x)
+
+#age=25
+
+#print("Mike's age is",age,".")
+
+
+#print(7/2)
+
+#print(int(7/2))
+
+print (7%2)
